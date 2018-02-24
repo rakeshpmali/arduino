dc846941cf580b81ed69e8e5336471f2e5ec9f0e
rakeshpmali <rakeshmali.in@gmail.com>
2018-02-24 20:01:40 +0530

    Added new samples

---
+++ RGB_LED/RGB_LED.ino
@@ -0,0 +1,47 @@
+/*
+Adafruit Arduino - Lesson 3. RGB LED
+*/
+
+int redPin = 10;
+int greenPin = 11;
+int bluePin = 12;
+
+//uncomment this line if using a Common Anode LED
+//#define COMMON_ANODE
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+}
+
+void loop()
+{
+  setColor(255, 0, 0);  // red
+  delay(500);
+  setColor(0, 255, 0);  // green
+  delay(500);
+  setColor(0, 0, 255);  // blue
+  delay(500);
+  /*setColor(255, 255, 0);  // yellow
+  delay(500);  
+  setColor(80, 0, 80);  // purple
+  delay(500);
+  setColor(0, 255, 255);  // aqua
+  delay(500);
+  setColor(0x4B, 0x0, 0x82);  // indigo
+  delay(500);*/
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
